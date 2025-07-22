a51d75fb52de29dcd1178f93553490c4464d2981
Busid <desarrolloweb214@gmail.com>
2025-07-22 02:23:39 +0200

    change environment production apiUrl

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: ''
+  apiUrl: 'https://booking-app-4drx.onrender.com'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://booking-app-4drx.onrender.com'
+  apiUrl: 'http://localhost:2000'
 };
